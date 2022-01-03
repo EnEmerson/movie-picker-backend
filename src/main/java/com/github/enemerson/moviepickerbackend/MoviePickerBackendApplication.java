b46dd4c422c52230184fc80670f02ea9cc945e63
En Emerson <007emersone@gmail.com>
2022-01-02 23:34:15 -0600

    feat: add spring boot stuff

---
+++ src/main/java/com/github/enemerson/moviepickerbackend/MoviePickerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.enemerson.moviepickerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePickerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviePickerBackendApplication.class, args);
+    }
+
+}
